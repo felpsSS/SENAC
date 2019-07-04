c79c5aa87ab23c09975ac6c6c8f1f9b32c7e2a0a
raul <51175595+felpsSS@users.noreply.github.com>
2019-07-04 16:58:10 -0300

    Add files via upload

---
+++ Aulas felipe/Banco de Dados/script e bds/primeiro CRUD.sql
@@ -0,0 +1,15 @@
+INSERT INTO aluno
+    (rg, escolaridade, email, nome, endereco)
+VALUES 
+    ('11,333.444-0', 'Cursando ensino medio', 'Fulano@fulano.com', 'beltrano', 'rua dos bobos');
+
+SELECT * FROM aluno;
+
+INSERT INTO aluno
+    (rg, escolaridade, email, nome, endereco)
+VALUES 
+    ('11,333.444-0',
+     'Cursando ensino medio', 
+     'raul.SJBV@gmail.com',
+      'Raul', 
+      'Rua Luiz Gonzaga de Godoy');
